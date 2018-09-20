8fdbae3b3155108c4e8dde789ff79346c6b9ccc2
liuanxin <liu_anxin@163.com>
2018-09-20 16:50:20 +0800

    fix

--- mall-common/src/main/java/com/github/common/http/HttpOkClientUtil.java
+++ mall-common/src/main/java/com/github/common/http/HttpOkClientUtil.java
@@ -217,8 +217,8 @@ public class HttpOkClientUtil {
                 }
             }
         } catch (IOException e) {
-            if (LogUtil.ERROR_LOG.isInfoEnabled()) {
-                LogUtil.ERROR_LOG.info(String.format("(%s %s) exception", method, url), e);
+            if (LogUtil.ROOT_LOG.isInfoEnabled()) {
+                LogUtil.ROOT_LOG.info(String.format("(%s %s) exception", method, url), e);
             }
         }
         return null;
@@ -243,8 +243,8 @@ public class HttpOkClientUtil {
                 }
             }
         } catch (IOException e) {
-            if (LogUtil.ERROR_LOG.isInfoEnabled()) {
-                LogUtil.ERROR_LOG.info(String.format("download (%s) to file(%s) exception", url, file), e);
+            if (LogUtil.ROOT_LOG.isInfoEnabled()) {
+                LogUtil.ROOT_LOG.info(String.format("download (%s) to file(%s) exception", url, file), e);
             }
         }
     }
