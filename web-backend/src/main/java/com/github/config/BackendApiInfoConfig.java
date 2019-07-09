46819e05c82fe4b49890ad7711cc50cc4899ad4a
liujie <liu_anxin@163.com>
2019-07-09 15:28:01 +0800

    fix

--- web-backend/src/main/java/com/github/config/BackendApiInfoConfig.java
+++ web-backend/src/main/java/com/github/config/BackendApiInfoConfig.java
@@ -1,9 +1,13 @@
 package com.github.config;
 
+import com.github.common.Const;
 import com.github.common.json.JsonCode;
+import com.github.common.mvc.AppVersion;
 import com.github.global.constant.Develop;
 import com.github.liuanxin.api.annotation.EnableApiInfo;
+import com.github.liuanxin.api.annotation.ParamType;
 import com.github.liuanxin.api.model.DocumentCopyright;
+import com.github.liuanxin.api.model.DocumentParam;
 import com.github.liuanxin.api.model.DocumentResponse;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -12,6 +16,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -32,6 +37,7 @@ public class BackendApiInfoConfig {
                 .setVersion(Develop.VERSION)
                 .setOnline(online)
                 .setIgnoreUrlSet(ignoreUrl())
+                .setGlobalTokens(tokens())
                 .setGlobalResponse(globalResponse());
     }
 
@@ -45,5 +51,12 @@ public class BackendApiInfoConfig {
             responseList.add(new DocumentResponse(code.getFlag(), code.getMsg()));
         }
         return responseList;
+    }
+
+    private List<DocumentParam> tokens() {
+        return Arrays.asList(
+                DocumentParam.buildToken(Const.VERSION, "认证数据", "abc-xyz", ParamType.Header).setHasTextarea(true),
+                DocumentParam.buildToken(Const.VERSION, "接口版本", AppVersion.currentVersion(), ParamType.Query).setMust(true)
+        );
     }
 }
