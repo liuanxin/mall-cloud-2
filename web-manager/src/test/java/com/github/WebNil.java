445f23d0c8be6cae0111fceb00adabd79bf4bfe1
liujie <liu_anxin@163.com>
2019-09-04 13:22:08 +0800

    fix

--- web-manager/src/test/java/com/github/WebNil.java
+++ web-manager/src/test/java/com/github/WebNil.java
@@ -4,7 +4,8 @@ import com.github.common.util.Compressor;
 
 public class WebNil {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // String str = Files.asCharSource(new File("/home/admin/api-info-en.html"), StandardCharsets.UTF_8).read();
         String str = "";
         System.out.println(Compressor.html(str));
     }
