2a690ce25f6f8bbd5a9b26633905e36a36957070
liujie <liu_anxin@163.com>
2019-10-31 17:40:45 +0800

    fix

--- web-manager/src/test/java/com/github/WebNil.java
+++ web-manager/src/test/java/com/github/WebNil.java
@@ -1,29 +1,27 @@
 package com.github;
 
-import com.github.common.util.A;
-import com.github.common.util.Compressor;
-import com.google.common.io.Files;
-
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-
 public class WebNil {
 
     public static void main(String[] args) throws Exception {
-        String path = "/home/admin/project/github/api-document/src/main/resources/static/";
-        Map<String, String> files = A.maps(
-                "intact-api-info.html", "api-info.html",
-                "intact-api-info-example.html", "api-info-example.html",
+//        String str = "";
+//        System.out.println(Compressor.html(str));
 
-                "intact-api-info-en.html", "api-info-en.html",
-                "intact-api-info-en-example.html", "api-info-en-example.html"
-        );
-        for (Map.Entry<String, String> entry : files.entrySet()) {
-            String content = Files.asCharSource(new File(path, entry.getKey()), StandardCharsets.UTF_8).read();
-            String compress = Compressor.html(content) + "\n";
-            Files.write(compress.getBytes(StandardCharsets.UTF_8), new File(path, entry.getValue()));
-            //Files.asByteSink(new File(path, entry.getValue())).write(compress.getBytes(StandardCharsets.UTF_8));
-        }
+//        String path = "/home/tony/project/github/api-document/src/main/resources/static/";
+//        Map<String, String> files = A.maps(
+//                "intact-api-info.html", "api-info.html",
+//                "intact-api-info-example.html", "api-info-example.html",
+//
+//                "intact-api-info-en.html", "api-info-en.html",
+//                "intact-api-info-en-example.html", "api-info-en-example.html"
+//        );
+//        for (Map.Entry<String, String> entry : files.entrySet()) {
+//            String content = Files.asCharSource(new File(path, entry.getKey()), StandardCharsets.UTF_8).read();
+//            String compress = Compressor.html(content) + "\n";
+//            Files.write(compress.getBytes(StandardCharsets.UTF_8), new File(path, entry.getValue()));
+//            //Files.asByteSink(new File(path, entry.getValue())).write(compress.getBytes(StandardCharsets.UTF_8));
+//        }
+
+        System.out.println(String.class.isAssignableFrom(Object.class));
+        System.out.println(Object.class.isAssignableFrom(String.class));
     }
 }
