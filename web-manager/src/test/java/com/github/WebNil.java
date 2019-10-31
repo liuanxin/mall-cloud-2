b67013358bc4fa2f61757642f010e747b8b4e629
liujie <liu_anxin@163.com>
2019-10-31 18:24:04 +0800

    fix

--- web-manager/src/test/java/com/github/WebNil.java
+++ web-manager/src/test/java/com/github/WebNil.java
@@ -1,27 +1,11 @@
 package com.github;
 
+import com.github.common.util.Compressor;
+
 public class WebNil {
 
     public static void main(String[] args) throws Exception {
-//        String str = "";
-//        System.out.println(Compressor.html(str));
-
-//        String path = "/home/tony/project/github/api-document/src/main/resources/static/";
-//        Map<String, String> files = A.maps(
-//                "intact-api-info.html", "api-info.html",
-//                "intact-api-info-example.html", "api-info-example.html",
-//
-//                "intact-api-info-en.html", "api-info-en.html",
-//                "intact-api-info-en-example.html", "api-info-en-example.html"
-//        );
-//        for (Map.Entry<String, String> entry : files.entrySet()) {
-//            String content = Files.asCharSource(new File(path, entry.getKey()), StandardCharsets.UTF_8).read();
-//            String compress = Compressor.html(content) + "\n";
-//            Files.write(compress.getBytes(StandardCharsets.UTF_8), new File(path, entry.getValue()));
-//            //Files.asByteSink(new File(path, entry.getValue())).write(compress.getBytes(StandardCharsets.UTF_8));
-//        }
-
-        System.out.println(String.class.isAssignableFrom(Object.class));
-        System.out.println(Object.class.isAssignableFrom(String.class));
+        String str = "";
+        System.out.println(Compressor.html(str));
     }
 }
