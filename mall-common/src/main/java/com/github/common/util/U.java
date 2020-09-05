e52fc7d455e593080c3e71410e7dfef563e76ec9
TY Liu <liu.anxin13@gmail.com>
2020-09-05 14:47:17 +0800

    fix

--- mall-common/src/main/java/com/github/common/util/U.java
+++ mall-common/src/main/java/com/github/common/util/U.java
@@ -40,7 +40,7 @@ public final class U {
     /** IPv4 地址 */
     private static final String IPV4 = "^([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])(\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])){3}$";
     /** 身份证号码 */
-    private static final String ID_CARD = "^([0-9]{15}|[0-9]{17}[0-9Xx])$";
+    private static final String ID_CARD = "(^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}$)";
 
     /** 字母 */
     private static final String LETTER = "(?i)[a-z]";
