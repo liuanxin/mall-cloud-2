c3060c6958170613dacf39b7d540e13f64321ffc
Jie <zt17947@ztn.com>
2021-06-23 16:29:04 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/FeignInterceptor.java
+++ mall-global/src/main/java/com/github/global/config/FeignInterceptor.java
@@ -1,35 +1,36 @@
-package com.github.global.config;
-
-import com.github.common.util.RequestUtils;
-import com.google.common.collect.Sets;
-import feign.Feign;
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.Set;
-
-@Configuration
-@ConditionalOnClass({HttpServletRequest.class, FeignClient.class, Feign.class})
-public class FeignInterceptor implements RequestInterceptor {
-
-    private static final Set<String> IGNORE_HEADER_SET = Sets.newHashSet(
-            "content-length", "content-type", "accept"
-    );
-
-    @Override
-    public void apply(RequestTemplate template) {
-        HttpServletRequest request = RequestUtils.getRequest();
-        Enumeration<String> headers = request.getHeaderNames();
-        while (headers.hasMoreElements()) {
-            String headName = headers.nextElement();
-            if (!IGNORE_HEADER_SET.contains(headName.toLowerCase())) {
-                template.header(headName, request.getHeader(headName));
-            }
-        }
-    }
-}
+//package com.github.global.config;
+//
+//import com.github.common.util.RequestUtils;
+//import com.google.common.collect.Sets;
+//import feign.Feign;
+//import feign.RequestInterceptor;
+//import feign.RequestTemplate;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.Enumeration;
+//import java.util.Set;
+//
+//@Configuration
+//@ConditionalOnClass({HttpServletRequest.class, FeignClient.class, Feign.class})
+//public class FeignInterceptor implements RequestInterceptor {
+//
+//    private static final Set<String> IGNORE_HEADER_SET = Sets.newHashSet(
+//            "content-length", "content-type", "accept"
+//    );
+//
+//    @Override
+//    public void apply(RequestTemplate template) {
+//        HttpServletRequest request = RequestUtils.getRequest();
+//        Enumeration<String> headers = request.getHeaderNames();
+//        while (headers.hasMoreElements()) {
+//            String headName = headers.nextElement();
+//            if (!IGNORE_HEADER_SET.contains(headName.toLowerCase())) {
+//                template.header(headName, request.getHeader(headName));
+//            }
+//        }
+//        // template.header(Const.TRACE, RequestUtils.getCookieOrHeaderOrParam(Const.TRACE));
+//    }
+//}
