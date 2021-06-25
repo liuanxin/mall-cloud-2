cdc68f3aa1a5fd20929e322fc28cf812de9020a4
Jie <zt17947@ztn.com>
2021-06-25 15:57:27 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/FeignInterceptor.java
+++ mall-global/src/main/java/com/github/global/config/FeignInterceptor.java
@@ -1,6 +1,5 @@
 //package com.github.global.config;
 //
-//import com.github.common.util.RequestUtils;
 //import com.google.common.collect.Sets;
 //import feign.Feign;
 //import feign.RequestInterceptor;
@@ -8,6 +7,9 @@
 //import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 //import org.springframework.cloud.openfeign.FeignClient;
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.context.request.RequestAttributes;
+//import org.springframework.web.context.request.RequestContextHolder;
+//import org.springframework.web.context.request.ServletRequestAttributes;
 //
 //import javax.servlet.http.HttpServletRequest;
 //import java.util.Enumeration;
@@ -23,14 +25,19 @@
 //
 //    @Override
 //    public void apply(RequestTemplate template) {
-//        HttpServletRequest request = RequestUtils.getRequest();
-//        Enumeration<String> headers = request.getHeaderNames();
-//        while (headers.hasMoreElements()) {
-//            String headName = headers.nextElement();
-//            if (!IGNORE_HEADER_SET.contains(headName.toLowerCase())) {
-//                template.header(headName, request.getHeader(headName));
+//        // 在项目拦截器中需要设置子线程共用
+//        // RequestContextHolder.setRequestAttributes(RequestContextHolder.getRequestAttributes(), true);
+//        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+//        if (requestAttributes instanceof ServletRequestAttributes) {
+//            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+//            Enumeration<String> headers = request.getHeaderNames();
+//            while (headers.hasMoreElements()) {
+//                String headName = headers.nextElement();
+//                if (!IGNORE_HEADER_SET.contains(headName.toLowerCase())) {
+//                    template.header(headName, request.getHeader(headName));
+//                }
 //            }
+//            // template.header(Const.TRACE, RequestUtils.getCookieOrHeaderOrParam(Const.TRACE));
 //        }
-//        // template.header(Const.TRACE, RequestUtils.getCookieOrHeaderOrParam(Const.TRACE));
 //    }
 //}
